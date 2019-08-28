520e88b61f574ecef97cb0c1d6607cf84d88d523
Souvikcse <sayanwinz@gmail.com>
2019-08-28 23:17:13 +0530

    preventing sql injection
    Signed-off-by: Souvikcse <sayanwinz@gmail.com>

--- mysql-express-server/db.js
+++ mysql-express-server/db.js
@@ -8,5 +8,35 @@ const connection = mysql.createConnection({
 })
 
 function getpersons() {
-    
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `SELECT * FROM Persons`,
+            function (err, rows, cols){
+                if(err){
+                    reject(err)
+                }
+                else{
+                    resolve(rows)
+                }
+            })
+    })
+}
+function addPerson(name, age, city) {
+    return new Promise((resolve, reject) => {
+        connection.query(
+            `INSERT INTO Persons(name, age, city) VALUES(?, ?, ?)`, [name, age, city],
+            function(err, results){
+                if(err){
+                    reject(err)
+                }
+                else{
+                    resolve();
+                }
+            }
+        )
+    })
+  }
+
+module.exports = {
+    getpersons, addPerson
 }
--- mysql-express-server/server.js
+++ mysql-express-server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express();
+const db = require('./db')
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
@@ -7,9 +8,11 @@ app.use(express.urlencoded({extended: true}));
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views')
 app.get('/', (req, res) => {
-    res.render('persons', {
-        persons: [{name: 'Souvik', age: 22, city: 'Kolkata'},
-                    {name: 'Jitendra', age: 22, city: 'Jadavpur'}]
+    db.getpersons()
+    .then((persons) => {
+        res.render('persons', {persons})
+    }).catch((err) => {
+        res.send(err)
     })
 })
 
@@ -17,7 +20,13 @@ app.get('/add', (req, res) => {
     res.render('add_persons')
 })
 app.post('/add', (req, res) => {
-    
+    db.addPerson(req.body.name, req.body.age, req.body.city)
+    .then(() => {
+        res.redirect('/')
+    })
+    .catch((err) =>{
+        res.send(err)
+    })
 })
 
 app.listen(9700);
